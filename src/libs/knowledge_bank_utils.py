0d0e9494a9036af89a5743977497fd0ab41bf515
Chengyu <huangchengyu16@gmail.com>
2019-03-05 22:59:12 -0500

    update match rule add place holder

--- src/libs/knowledge_bank_utils.py
+++ src/libs/knowledge_bank_utils.py
@@ -9,7 +9,8 @@ Created on Sat Mar  2 15:46:04 2019
 import pandas as pd 
 import numpy as np
 import re 
-
+import jieba
+import copy
 #%%
 
 def read_sets(file_name,sheet_name):
@@ -32,7 +33,7 @@ def read_pattern(file_name,sheet_name,id_column,pattern_column):
     #print(id_pattern_pair[1])
     return id_pattern_pair
 
-def process_pattern(p,set_dict):
+def process_pattern(p,set_dict,place_holder_dict=None):
     res = p.split('#')
     res = [detect_set(i.strip()) for i in res if i is not '']
     set_key_list = set_dict.keys()
@@ -45,7 +46,13 @@ def process_pattern(p,set_dict):
                     print('\nwaring, {} not in set list:{}\n'.format(s,p))
                     yield None
         else:
-            yield r
+            if place_holder_dict:
+                if r in place_holder_dict.keys():
+                    yield place_holder_dict[r]
+                else:
+                    yield r
+            else:
+                yield r
             
 def detect_set(p):
     setRegex = re.compile(r'<set>(.*?)</set>')
@@ -55,10 +62,10 @@ def detect_set(p):
     else:
         return p 
 
-def convert2record_list(id_pattern_pair,set_dict):
+def convert2record_list(id_pattern_pair,set_dict,place_holder_dict=None):
     res_dict = {'id':id_pattern_pair[0],
                 'pattern':id_pattern_pair[1],
-                'match_list': list(process_pattern(id_pattern_pair[1],set_dict))}
+                'match_list': list(process_pattern(id_pattern_pair[1],set_dict,place_holder_dict))}
     return res_dict
 
 def match_element(input_word,record):
@@ -78,21 +85,27 @@ def match_element(input_word,record):
     return False 
 
 def _match_pattern(input_list,record):
-       res = [match_element(i,record) for i in input_list]
-       true_count = sum(res)
-       input_length = len(res)
-       record_length = len(record['match_list'])
-       res = {'input_score':true_count/input_length,
-              'pattern_match_score':true_count/record_length}
-       return res 
+    record_copy = copy.copy(record)
+    res = [match_element(i,record) for i in input_list]
+    true_count = sum(res)
+    input_length = len(res)
+    record_length = len(record['match_list'])
+    record_copy['input_score']=true_count/input_length
+    record_copy['pattern_match_score']=true_count/record_length
+
+#    res = {'input_score':true_count/input_length,
+#          'pattern_match_score':true_count/record_length}
+    return record_copy 
    
-def match_patterns(input_list,record_list,thresh=1):
-    res_list = [(r,_match_pattern(input_list,r)) for r in record_list]
-    res_list = [r for r in res_list if r[1]['pattern_match_score']>=thresh]
+def match_patterns(input_list,record_list,input_thresh=0.5,pattern_thresh=0.5):
+    #res_list = [(r,_match_pattern(input_list,r)) for r in record_list]
+    res_list = [_match_pattern(input_list,r) for r in record_list]
+    res_list = [r for r in res_list if r['pattern_match_score']>=pattern_thresh]
+    res_list = [r for r in res_list if r['input_score']>=input_thresh]
     if len(res_list) == 0 :
         return None
     
-    res_sorted = sorted(res_list, key = lambda x: (x[-1]['pattern_match_score'], x[-1]['input_score']),reverse=True)
+    res_sorted = sorted(res_list, key = lambda x: ( x['input_score'],x['pattern_match_score']),reverse=True)
     
     return res_sorted
 
@@ -100,15 +113,18 @@ def check_dups(inpuyt_list):
     return None
 
 #%%
+if __name__ == "__main__":
+    ## load data and pattern
+    kb_path = "../../data/raw/knowledge_input.xlsx"
+    set_dict = read_sets(kb_path,'sets')
+    place_holder_dict = read_sets(kb_path,'place_holder')
+    id_pattern_pairs = read_pattern(kb_path,'ask_pattern','Unnamed: 1','pattern')
+    record_list = [convert2record_list(idpp,set_dict,place_holder_dict) for idpp in id_pattern_pairs]
+    ## run one test 
+    test = "你叫什么名字？"
+    test = list(jieba.cut(test))
+    res = match_patterns(test,record_list,0.6,0.6)
+    res = match_patterns(test,res,0.6,0.7)
+    
+    print(res)
 
-kb_path = "../../data/raw/knowledge_input.xlsx"
-set_dict = read_sets(kb_path,'sets')
-id_pattern_pairs = read_pattern(kb_path,'ask_pattern','Unnamed: 1','pattern')
-record_list = [convert2record_list(idpp,set_dict) for idpp in id_pattern_pairs]
-
-#%%
-for r in record_list:
-    try:
-        _match_pattern(test,r)
-    except:
-        print(r)
