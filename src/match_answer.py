e3883fcda57a7feb0744622aa39e73d4a9a91853
Chengyu <huangchengyu16@gmail.com>
2019-03-16 22:22:17 -0400

    add test results

--- src/match_answer.py
+++ src/match_answer.py
@@ -13,6 +13,7 @@ from knowledge_bank_utils import read_sets,read_pattern,convert2record_list,matc
 from hanlp_parse import han_analyzer
 from sentence_structure_utils import base_structure
 from input_process_util import Processor
+import pandas as pd
 #%%
 
 def get_dep_output_han(sentence,han_analyser):
@@ -46,22 +47,51 @@ def match(sentence,processor,analyzer,record_list,deep_match=False):
     ans = match_patterns(eles,record_list,0.6,0.7)
     if ans and deep_match:
         print('level > 2 info used for match')
-        ans = match_patterns(eles2,ans,0.6,0.7)
+        ans = match_patterns(eles2,ans,0.4,0.4)
     return ans
 
 #%%
-kb_path = "../data/raw/knowledge_input.xlsx"
-set_dict = read_sets(kb_path,'sets')
-place_holder_dict = read_sets(kb_path,'place_holder')
-id_pattern_pairs = read_pattern(kb_path,'ask_pattern','Unnamed: 1','pattern')
-record_list = [convert2record_list(idpp,set_dict,place_holder_dict) for 
-               idpp in id_pattern_pairs]
-#%%
-
-test_sentence = "你觉得你有什么用？"
-analyzer = han_analyzer()
-processor = Processor(init_stop_words_path='./libs/init_stop_words.txt')
-ans = match(test_sentence,processor,analyzer,record_list,deep_match=False)
-
-if ans:    
-    print(ans[0])
+if __name__ == "__main__":
+    kb_path = "../data/raw/knowledge_input.xlsx"
+    test_file_path = "../data/raw/mdl_test_data.xlsx"
+    set_dict = read_sets(kb_path,'sets')
+    place_holder_dict = read_sets(kb_path,'place_holder')
+    id_pattern_pairs = read_pattern(kb_path,'ask_pattern','Unnamed: 1','pattern')
+    record_list = [convert2record_list(idpp,set_dict,place_holder_dict) for 
+                   idpp in id_pattern_pairs]
+    #%%
+    test_df = pd.read_excel(test_file_path,'test_corpus')
+    test_data = test_df['input'].tolist()
+    #%%
+    
+    analyzer = han_analyzer()
+    processor = Processor(init_stop_words_path='./libs/init_stop_words.txt')
+    
+    #%%
+    # run one test sentence 
+    test_sentence = "你觉得你有什么用？"
+    ans = match(test_sentence,processor,analyzer,record_list,deep_match=False)
+    
+    if ans:    
+        print(ans[0])
+        
+    #%%
+    res = []
+    res_deep = []
+    for t in test_data:
+        ans = match(t,processor,analyzer,record_list,deep_match=False)
+        ans_deep = match(t,processor,analyzer,record_list,deep_match=True)
+        if ans:
+            res.append(ans[0]['id'])
+        else:
+            res.append(ans)
+        
+        if ans_deep:
+            res_deep.append(ans_deep[0]['id'])
+        else:
+            res_deep.append(ans_deep)
+    #%%
+    df_data = zip(test_data,test_df['test_category'].tolist(),res,res_deep)
+    out_df = pd.DataFrame(df_data,columns=['input','test_category','res','res_deep'])
+    #%%
+    out_df.to_excel('../data/results/match_res.xlsx')
