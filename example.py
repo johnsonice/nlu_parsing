5d9b9f30ee7d9f8a29229c02da7409a8f8819831
Chengyu Huang <huangchengyu16@gmail.com>
2019-06-21 15:35:54 -0400

    change

--- example.py
+++ example.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     nlu = RB2(kb_path,init_stop_words_path,chatbot_keywords_path)
     #%%
     # run one example 
-    test_sentence= "你好啊，你会做什么事情么"
+    test_sentence= "你会做什么事情么"
     ans = nlu.match(test_sentence,deep_match=True,match_intent=False,topn=1,check_long_sentence=True)   
     print(ans)
 #%%
